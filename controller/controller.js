3a3213b340e601fc5fdd9b15649425b36333d7d3
Nick Bansal <extreme_wired@msn.com>
2018-10-05 14:22:50 +0100

    Promise All

--- controller/controller.js
+++ controller/controller.js
@@ -3,8 +3,8 @@ const spanishNews = require('../news/index.js');
 exports.sendTodaysNews = (req, res, next) => {
   spanishNews()
   .then(news => {
-    console.log(news);
-    res.send(news.translations[0].translation);
+    // console.log(news)
+    res.send({ news });
   })
   .catch(err => {
     console.log(err)
--- news/index.js
+++ news/index.js
@@ -13,16 +13,27 @@ const spanishNews = () => {
       language: 'en'
       // country: 'us'
     })
-
-    
     .then(response => {
+      console.log(response);
       const title = response.articles[0].title;
       const content = response.articles[0].content;
-    
-      // body.forEach(el => {
-        return generateTranslate(title);
-      // })
+      const urlImage = response.articles[0].urlToImage
+      const description = response.articles[0].description;
+      return Promise.all([
+        generateTranslate(title), 
+        generateTranslate(content),
+        generateTranslate(urlImage),
+        generateTranslate(description)
+      ])
     })
+    .then(values => {
+      const title = values[0].translations[0].translation
+      const content = values[1].translations[0].translation
+      const urlImage = values[2].translations[0].translation
+      const description = values[3].translations[0].translation
+      return {title, content, urlImage, description}
+    })
+
 };
 
 module.exports = spanishNews;
